b5a08cf361f7b9500204846011455255f2d1044b
KaJoker <medinfo94@yahoo.com>
2016-03-29 22:28:59 +0200

    Amélioration et integration de script: question3 _ l'état de phpmyadmin et apache2

--- bin/phpmyadmin.sh
+++ bin/phpmyadmin.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
+chaine1="truc"
+chaine2="truc"
 IP=$(ifconfig eth0 | grep 'inet adr:' | cut -d: -f 2 | awk '{print $1}')
-curl $IP/phpmyadmin 1>php_ret 2>/dev/null # on met le résultat dans un fichier 
-chaine=$(grep -o -w "404 Not Found" php_ret ) # on cherche la chaine 404 Not Found 
-if [ "$chaine" = "404 Not Found" ] 
+curl $IP/phpmyadmin 1>php_ret 2>ERR # on met le résultat dans un fichier 
+chaine1=$(grep -o -w "404 Not Found" php_ret ) # on cherche la chaine 404 Not Found 
+chaine2=$(grep -o -w "Failed to connect to" ERR)
+
+if [ "$chaine1" = "404 Not Found" ] || [ "$chaine2" = "Failed to connect to" ] 
      then echo KO 
      else echo OK 
 fi
