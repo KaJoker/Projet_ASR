9e3a71f26f661405385900b12a5e4dd4ef290cc4
KaJoker <medinfo94@yahoo.com>
2016-03-29 23:08:56 +0200

    Toujours des problms au démarrage des service sql et apache2

--- bin/question5.sh
+++ bin/question5.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#service mysql stop 
+service mysql stop 
 #service apache2 stop
 sed -i  's/export APACHE_RUN_USER=www-data/export APACHE_RUN_USER=carl/g' /etc/apache2/envvars 
-sed -i  's/export APACHE_RUN_USER=www-data/export APACHE_RUN_GROUP=carl/g' /etc/apache2/envvars 
+sed -i  's/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=carl/g' /etc/apache2/envvars 
 
 echo "500" > /var/run/apache2/apache2$SUFFIX.pid
 sed -i 's/= mysql/= beth/g' /etc/mysql/my.cnf  
@@ -17,6 +17,6 @@ MY_USER=$(grep -i "user\s" /etc/mysql/my.cnf | cut -d"=" -f 2 | awk '{print $1}'
 echo "APACHE_USER: $AP_USER | APACHE_PID : $pid_apc"
 echo "MYSQL_USER : $MY_USER | MYSQL_PID  : $pid_sql"  
 
-#service mysql start 
-#service apache2 start
-#mysql -ppassmysql
+service mysql start 
+service apache2 start
+mysql -ppassmysql
