4ffd4d33b4547b1547c51d95bae661660b986d9f
kajoker <medinfo94@yahoo.com>
2016-03-30 09:50:18 +0200

    Amélioration de la 2éme question

--- bin/install_all.sh
+++ bin/install_all.sh
@@ -1,10 +1,10 @@
 #! /bin/bash 
 
-echo "\n--- Installation de mysql et phpmyadmin ---\n" 
+echo "\n--- Installation de mysql et phpmyadmin et apache2 ---\n" 
 echo "Recherche de mis-à-jour" 
 apt-get	update  
-echo "mysql-server mysql-server/root_password password passmysql" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password passmysql" | debconf-set-selections
+echo "mysql-server mysql-server/root_password password glenn" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password glenn" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/app-password password passphp" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/app-password-confirm password passphp" | debconf-set-selections
@@ -12,5 +12,4 @@ echo "phpmyadmin phpmyadmin/mysql/admin-pass password passphp" | debconf-set-sel
 echo "phpmyadmin phpmyadmin/mysql/app-pass password passphp" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
 apt-get -y install phpmyadmin mysql-server 
-echo -e "\n--- Installation de curl ---\n"
 apt-get -y install curl > /dev/null 2>&1 
