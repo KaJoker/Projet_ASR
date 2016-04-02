b86fa7de1cab58abf92b6331c38dcf63f75c5664
KaJoker <medinfo94@yahoo.com>
2016-04-02 10:08:48 +0200

    changement de méthode pour detecter les utilisateurs

--- BIN_v2.0/question3.sh
+++ BIN_v2.0/question3.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
 
-echo "rick" > /etc/hostname 
+ 
 host=$(hostname)
 
 http_state=$( sh http_state.sh )
@@ -13,7 +13,7 @@ AP_USER=$(grep -i APACHE_RUN_USER= /etc/apache2/envvars | cut -d"=" -f 2 )
 
 MY_USER=$(grep -i "user" /etc/mysql/my.cnf | cut -d"#" -f 4 | awk '{print $3}' )
 
-pid_mysql=$(pidof -s mysql)
+
 
 pid_mysql2=$(ps -aux  | grep -w mysql | cut -d"?" -f 1 | awk '{print $2}' | grep -E -w "([0-9][0-9][0-9])")
 
@@ -26,4 +26,4 @@ echo 'phpmyadmin_state :'$phpmyadmin_state  >>information.txt
 echo 'http_user_conf :' $AP_USER >>information.txt
 echo 'Mysql_user_conf :' $MY_USER >>information.txt
 echo 'http_user_pid :'  $pid_http >>information.txt
-echo 'mysql_user_pid :' $pid_mysql >>information.txt
+echo 'mysql_user_pid :' $pid_mysql2 >>information.txt
