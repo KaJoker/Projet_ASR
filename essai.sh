e2524a2d4f5e599521d5f083cf6ff2176103ae19
KaJoker <medinfo94@yahoo.com>
2016-04-02 23:40:12 +0200

    General Folders and files update

--- essai.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-echo "ceci est un test."
--- phpmyadmin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-IP=$(ifconfig eth0 | grep 'inet adr:' | cut -d: -f 2 | awk '{print $1}')
-curl $IP/phpmyadmin 1>php_ret 2>/dev/null # on met le résultat dans un fichier 
-chaine=$(grep -o -w "404 Not Found" php_ret ) # on cherche la chaine 404 Not Found 
-if [ "$chaine" = "404 Not Found" ] 
-     then echo KO 
-     else echo OK 
-fi
