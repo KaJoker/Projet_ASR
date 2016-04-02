985123a85f9282595381f28378b6135a79ef14ab
KaJoker <medinfo94@yahoo.com>
2016-04-02 11:19:13 +0200

    supp L , pid_mysql et pas pid_mysql2

--- BIN_v2.0/question3.sh
+++ BIN_v2.0/question3.sh
@@ -11,11 +11,14 @@ phpmyadmin_state=$(sh phpmyadmin.sh)
 
 AP_USER=$(grep -i APACHE_RUN_USER= /etc/apache2/envvars | cut -d"=" -f 2 )
 
-MY_USER=$(grep -i "user" /etc/mysql/my.cnf | cut -d"#" -f 4 | awk '{print $3}' )
+#MY_USER=$(grep -i "user" /etc/mysql/my.cnf | cut -d"#" -f 4 | awk '{print $3}' )
 
+MY_USER=$(grep -i "user\s" /etc/mysql/my.cnf | cut -d"=" -f 2 | awk '{print $1}' | head -n 1 )
 
+#pid_mysql2=$(ps -aux  | grep -w mysql | cut -d"?" -f 1 | awk '{print $2}' | grep -E -w "([0-9][0-9][0-9])")
 
-pid_mysql2=$(ps -aux  | grep -w mysql | cut -d"?" -f 1 | awk '{print $2}' | grep -E -w "([0-9][0-9][0-9])")
+pid_mysql=$(ps -aux | grep -w beth | cut -d"?" -f 1 | awk '{print $2}' | sed -n '1p')
+pid_apache=$(ps -aux | grep -w carl | cut -d"?" -f 1 | awk '{print $2}' | sed -n '1p')
 
 pid_http=$(pidof -s apache2 | awk '{print $1}' ) 
 
@@ -26,4 +29,4 @@ echo 'phpmyadmin_state :'$phpmyadmin_state  >>information.txt
 echo 'http_user_conf :' $AP_USER >>information.txt
 echo 'Mysql_user_conf :' $MY_USER >>information.txt
 echo 'http_user_pid :'  $pid_http >>information.txt
-echo 'mysql_user_pid :' $pid_mysql2 >>information.txt
+echo 'mysql_user_pid :' $pid_mysql >>information.txt
