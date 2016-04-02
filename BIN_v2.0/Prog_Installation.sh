5413f228ee5cd6a918caaee0fea0d12d008aa9ca
KaJoker <medinfo94@yahoo.com>
2016-04-02 14:36:07 +0200

    ajout question7

--- BIN_v2.0/Prog_Installation.sh
+++ BIN_v2.0/Prog_Installation.sh
@@ -5,11 +5,14 @@ echo "Test des services avant l'installation :
                  				 a) MySql 
                           			 b) phpmyadmin 
 			  			 c) Apache "
+
 bash question3.sh
 cat information.txt 
 sleep 10 
 
+
 echo "Installation de : Mysql, Phpmyadmin , Apache" 
+
 
 bash install_phpmyadmin.sh install.conf 
 bash question1.sh 
@@ -17,6 +20,7 @@ echo "Test des services après l'installation :
                                                  a) MySql
                                                  b) phpmyadmin
                                                  c) Apache "
+
 bash question3.sh 
 cat information.txt 
 sleep 10
@@ -25,10 +29,10 @@ sleep 10
 echo "Changement automatique d'utilisateurs " 
 
 bash change_user.sh change.conf
+sleep 10 
 
 echo " Automatisation à partir d'une image inconnue "
 
-bash question7.sh 
+bash question7.sh info7.conf 
 
 bash question8.sh 
-
